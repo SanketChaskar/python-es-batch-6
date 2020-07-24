eb386dbc767120ac58df5ac7bd2b06a74325d58d
SanketChaskar <68579115+SanketChaskar@users.noreply.github.com>
2020-07-24 09:36:17 +0530

    Add files via upload

---
+++ day2 assignment.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[8]:
+
+
+print("Welcome  to letsupgrade python Essentials")
+
+
+# In[11]:
+
+
+print("'$is american currency"')
+
+
+# In[12]:
+
+
+print(" we can do, if we want")
+
+
+# In[16]:
+
+
+print("im\nsanket")
+
+
+# In[24]:
+
+
+name= "sanket"
+age= "19"
+marks= "95.67"
+print(" The name of person is", name, "marks is" , marks, "age" , age)
+
+
+# In[35]:
+
+
+name= "xyz"
+age= "19"
+
+
+# In[33]:
+
+
+
+type(marks)
+
+
+# In[34]:
+
+
+type(age)
+
+
+# In[39]:
+
+
+a=10
+b=12
+c=a*b
+print(c)
+
+
+# In[40]:
+
+
+d=a**b
+e=b/a
+f=d+e
+print(f)
+
+
+# In[41]:
+
+
+print(f%a)
+
+
+# In[42]:
+
+
+a1=25
+b1=50
+print(a1>b1)
+
+
+# In[43]:
+
+
+print(a1>~b1)
+
+
+# In[ ]:
+
+
+
+
